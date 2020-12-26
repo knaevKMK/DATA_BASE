9fbcd6fd6f86dcaedba115ca655ac79dd964f7dd
Krasimir Kanev <knev@mail.bg>
2020-12-26 21:48:38 +0200

    Update Exercise_Data Definition and Datatypes.sql
    Create Multy_table DB

--- MY SQL/Exercise_Data Definition and Datatypes.sql
+++ MY SQL/Exercise_Data Definition and Datatypes.sql
@@ -91,7 +91,7 @@ ADD PRIMARY KEY (`id`, `username`);
 
 /*EX_10*/
 ALTER TABLE users
-CHANGE COLUMN last_login_time last_login_time  TIME;
+CHANGE COLUMN last_login_time last_login_time TIME;
 
 /*EX_11*/
 ALTER TABLE users
@@ -99,7 +99,88 @@ DROP PRIMARY KEY,
 ADD PRIMARY KEY (id);
 
 
-/*EX_9*/
+/*EX_12*/
+DROP SCHEMA movies;
+CREATE DATABASE movies;
+USE movies;
+
+CREATE TABLE directors (
+id INT AUTO_INCREMENT ,
+director_name VARCHAR(30) NOT  NULL,
+notes TEXT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE genres(
+id INT AUTO_INCREMENT,
+genre_name VARCHAR(50) NOT NULL,
+notes TEXT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE categories(
+id INT AUTO_INCREMENT,
+category_name VARCHAR(50) NOT NULL,
+notes TEXT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE movies(
+id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR (50) NOT NULL,
+director_id  INT NOT NULL,
+copyright_year YEAR,
+lenght TIME,
+genre_id INT NOT NULL,
+category_id INT NOT NULL,
+rating INT NULL,
+notes TEXT NULL
+);
+
+INSERT INTO directors (director_name) VALUES 
+('Mel Gibson'), 
+('Will Smith'), 
+('Stiven King'), 
+('Silvester Staloun '), 
+('Luk Beson'); 
+
+INSERT INTO  genres (genre_name) VALUES 
+('action'),
+('musichol'),
+('documental'),
+('horror'),
+('comedy');
+
+INSERT INTO categories (category_name) VALUES
+('drama'),
+('animation'),
+('fantasy'),
+('historical'),
+('criminal');
+
+INSERT INTO movies (title, director_id,copyright_year,genre_id,category_id) VALUES 
+('Lethal Weapon',1 ,'1993',1,1),
+('Man In Black',2 ,'1996',1,3),
+('It',3 ,'1994',4,3),
+('Rambo',4 ,'1991',1,1),
+('Taxy',5 ,'1993',5,3);
+
+
+ALTER TABLE movies
+ADD CONSTRAINT fk_directors_id
+FOREIGN KEY (director_id)
+REFERENCES directors (id),
+ADD CONSTRAINT fk_genres_id
+FOREIGN KEY (genre_id)
+REFERENCES genres (id),
+ADD CONSTRAINT fk_category_id
+FOREIGN KEY  (category_id)
+REFERENCES categories (id);
+
+SELECT m.id, title, d.director_name AS Director, copyright_year AS `Year`,g.genre_name AS Genre,c.category_name AS CATEGORY FROM movies  m
+JOIN directors d , genres g, categories c 
+WHERE director_id = d.id AND genre_id=g.id AND category_id=c.id;
+
 /*EX_9*/
 /*EX_9*/
 /*EX_9*/
