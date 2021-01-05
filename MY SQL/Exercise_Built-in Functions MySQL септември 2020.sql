5b7c908527caaf49bf324782c77566dafed0947a
Krasimir Kanev <knev@mail.bg>
2021-01-05 22:30:18 +0200

    Update Exercise_Built-in Functions MySQL септември 2020.sql

--- MY SQL/Exercise_Built-in Functions MySQL септември 2020.sql
+++ MY SQL/Exercise_Built-in Functions MySQL септември 2020.sql
@@ -10,10 +10,13 @@ WHERE last_name LIKE '%ei%'
 ORDER BY employee_id;
 
 # EX_3
-
 SELECT fist_name FROM employees
 WHERE deprtment_id IN (3,10) 
 	AND YEAR (hire_year) BETWEEN 1995 AND 2005
 ORDER BY employee_id;
 
 
+#EX_4
+SELECT first_name, last_name FROM employees
+WHERE job_title NOT LIKE '%engineer%'
+ORDER BY employee_id;
