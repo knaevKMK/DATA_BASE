7ded67e1d584d76f43286499dad05935e548c373
Krasimir Kanev <knev@mail.bg>
2021-01-06 00:43:57 +0200

    Update Exercise_Built-in Functions MySQL септември 2020.sql

--- MY SQL/Exercise_Built-in Functions MySQL септември 2020.sql
+++ MY SQL/Exercise_Built-in Functions MySQL септември 2020.sql
@@ -50,3 +50,52 @@ SELECT  * FROM v_employees_hired_after_2000;
 SELECT first_name, last_name FROM employees
 WHERE CHAR_LENGTH(last_name, 5);
 
+#EX_10
+SELECT country_name, iso_code FROM countries
+WHERE country_name LIKE '%A%A%A%'
+ORDER BY iso_code ASC;
+
+#EX_11
+SELECT p.peak_name, r.river_name, 
+(LOWER(concat(p.peak_name, SUBSTRING(r.river_name,2)))) AS mix 
+FROM peak p , river r
+WHERE RIGHT (p.peak_name,1) = LEFT (r.river_name,1)
+ORDER BY mix ASC;
+
+#EX_12
+SELECT `name`, `start` FROM diablo
+WHERE YEAR(`start`) IN (2011,2012)
+ORDER BY `start`, `name`
+LIMIT 50;
+
+#EX_13
+SELECT user_name, SUBSTRING(email, LOCATE('@', email)+1) AS `Email Provider`
+FROM `user`
+ORDER BY `Email Provider`, `user_name`;
+
+#EX_14
+SELECT user_name, ip_address FROM `user`
+WHERE ip_address LIKE '&quot;___.1%.%.___&quot;.'
+ORDER BY user_name ASC;
+
+#EX_15
+SELECT `name`, 
+ (
+ CASE
+ WHEN HOUR(`start`) BETWEEN 0 AND 11 THEN 'Morning'
+ WHEN HOUR(`start`) BETWEEN 12 AND 17 THEN 'Afternoon'
+ ELSE 'Evening'
+ END
+ ) AS `Duration`,
+  (
+ CASE
+ WHEN `duration` BETWEEN 0 AND 3 THEN 'Extra Short'
+ WHEN `duration` BETWEEN 4 AND 6 THEN 'Short'
+ WHEN `duration` BETWEEN 7 AND 10 THEN 'Long'
+ ELSE 'Extra Long'
+ END
+ ) AS `Duration`
+FROM games;
+
+#EX_16
+
