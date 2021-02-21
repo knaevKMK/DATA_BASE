4a3804c406b674489e0a9cd96faee67f25df1eb4
Kr_Knev <kneav@students.softuni.bg>
2021-02-21 19:19:25 +0200

    Practice

--- SPRING/dbAccessWithJDBC/src/Homework.java
+++ SPRING/dbAccessWithJDBC/src/Homework.java
@@ -2,39 +2,64 @@ import java.sql.*;
 import java.util.Properties;
 
 public class Homework {
-    public static final String CONNECTION_STRING =
-            "jdbc:mysql://localhost:3306/";
-    public static final String MINIONS_TABLE_NAME =
-            "minions_db";
+    public  static final    String CONNECTION_STRING="jdbc:mysql://localhost:3306/";
+    public static final String MINIONS_TABLE_NAME= "minions_db";
+
     Connection connection;
 
     public void setConnection(String user, String password) throws SQLException {
         Properties properties = new Properties();
-        properties.setProperty("user", user);
+        properties.setProperty("user",user);
         properties.setProperty("password", password);
 
-        connection = DriverManager
-                .getConnection(CONNECTION_STRING + MINIONS_TABLE_NAME, properties);
-
+        connection= DriverManager.getConnection(CONNECTION_STRING+MINIONS_TABLE_NAME,properties);
     }
 
     public void getVillansNamesEx2() throws SQLException {
-        String query = "SELECT v.name, COUNT(mv.minion_id) AS 'count'\n" +
+        String quety= "SELECT v.name, COUNT(mv.minion_id) AS 'count'\n" +
                 "FROM villains AS v\n" +
                 "JOIN minions_villains AS mv\n" +
                 "ON v.id = mv.villain_id\n" +
                 "GROUP BY v.id\n" +
                 "HAVING count> 15\n" +
                 "ORDER BY count DESC";
-
-
-        PreparedStatement stm = connection.prepareStatement(query);
-        ResultSet rs = stm.executeQuery();
-        while (rs.next()) {
-
-            System.out.printf("%s %d%n"
-                    , rs.getString("name")
-                    , rs.getInt(2));
+        PreparedStatement stm= connection.prepareStatement(quety);
+        ResultSet rs= stm.executeQuery();
+        while (rs.next()){
+            System.out.printf("%s %d%n", rs.getString("name"), rs.getInt(2));
         }
+    }
+    public void Ex3(int villainId) throws SQLException {
+        String villainName = getEntityNameById(villainId, "villains");
+        if (villainName == null) {
+            System.out.println("Villain does not exist!!! Not valid id:" + villainId);
+            return;
+        }
+        System.out.println("Villain: "+ villainName);
+
+        String query = ("SELECT m.name, m.age FROM minions AS m\n" +
+                "JOIN minions_villains AS mv ON m.id = mv.minion_id\n" +
+                "WHERE mv.villain_id=?");
+        PreparedStatement stm = connection.prepareStatement(query);
+        stm.setInt(1,villainId);
+        ResultSet resultSet = stm.executeQuery();
+
+        int count=1;
+        while (resultSet.next()){
+            System.out.printf("%d. %s %d%n"
+                    ,count++
+                    ,resultSet.getString("name")
+                    ,resultSet.getInt("age"));
+
+        }
+    }
+
+    private String getEntityNameById(int entityId, String table) throws SQLException {
+        String query = String.format("SELECT name FROM %s WHERE id = ?", table);
+        PreparedStatement pstm = connection.prepareStatement(query);
+        pstm.setInt(1, entityId);
+        ResultSet resultSet = pstm.executeQuery();
+
+        return resultSet.next() ? resultSet.getString("name") : null;
     }
 }
--- SPRING/dbAccessWithJDBC/src/Main.java
+++ SPRING/dbAccessWithJDBC/src/Main.java
@@ -1,13 +1,58 @@
 import com.mysql.cj.x.protobuf.MysqlxDatatypes;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        //MysqlxDatatypes.Scalar
-            Homework homework = new Homework();
-            homework.setConnection("root", "c!!2211paKev");
-            homework.getVillansNamesEx2();
+    private static Scanner scan = new Scanner(System.in);
 
+    public static void main(String[] args) throws SQLException {
+//        MysqlxDatatypes.Scalar;
+        System.out.println("Please insert log-in MYSQL server!");
+        System.out.println("user:");
+        String user = scan.nextLine();
+        System.out.println("password: ");
+        String password = scan.nextLine();
+
+
+        Homework homework = new Homework();
+       homework.setConnection(user, password);
+        System.out.println("Connecting....");
+
+        System.out.println("Please, choose number of Exercise: between 2 to 9"
+                + System.lineSeparator()
+                + " OR 'end' to Stop");
+
+        String read = scan.nextLine();
+        while (!"end".equalsIgnoreCase(read)) {
+            switch (read) {
+                case "2":
+                    homework.getVillansNamesEx2();
+                    break;
+                case "3":
+                    homework.Ex3(Integer.parseInt(scan.nextLine()));
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    break;
+                case "7":
+                    break;
+                case "8":
+                    break;
+                case "9":
+                    break;
+                default:
+                    System.out.println("Wrong choose! ");
+                    break;
+            }
+            System.out.println("Please, choose number of Exercise: between 2 to 9"
+                    + System.lineSeparator()
+                    + " OR 'end' to Stop");
+
+            read = scan.nextLine();
+        }
     }
 }
