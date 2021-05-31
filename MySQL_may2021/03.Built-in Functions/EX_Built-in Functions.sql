413e42bd98ff8eb06b003a98290e8303436fca1a
Kr_Knev <kneav@students.softuni.bg>
2021-05-31 21:35:54 +0300

    add

--- MySQL_may2021/03.Built-in Functions/EX_Built-in Functions.sql
+++ MySQL_may2021/03.Built-in Functions/EX_Built-in Functions.sql
@@ -101,3 +101,9 @@ ELSE 'Extra Long'
 END
 ) AS `Duration`
  FROM games;
+ 
+ #EX_16
+ 
+ SELECT product_name,order_date,
+ ADDDATE(order_date, interval 3 DAY)AS pay_due,
+ ADDDATE(order_date, INTERVAL 1 MONTH) AS deliver_due FROM orders;
