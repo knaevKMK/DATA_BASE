258ca02a7e9958ec49e11ab7e8b620e29d004adc
Kr_Knev <kneav@students.softuni.bg>
2021-06-08 00:10:28 +0300

    homework

---
+++ MySQL_may2021/05. Table-relation/Table_relation_Ex.sql
@@ -0,0 +1,219 @@
+CREATE SCHEMA demo_table_relation;
+USE demo_table_relation;
+
+#EX_1
+CREATE TABLE passports(
+passport_id INT AUTO_INCREMENT PRIMARY KEY,
+passport_number  VARCHAR(20)
+) AUTO_INCREMENT=101;
+
+CREATE TABLE people(
+person_id INT AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(20),
+salary DECIMAL(8,2) ,
+passport_id INT NOT NULL UNIQUE,
+CONSTRAINT fr_passport_id
+FOREIGN KEY (passport_id)
+REFERENCES passports (passport_id)
+);
+
+INSERT INTO passports (passport_number) VALUES
+('N34FG21B'),
+('K65LO4R7'),
+('ZE657QP2');
+
+INSERT INTO people (first_name,salary,passport_id) VALUES
+('Roberto', 43300.00, 102),
+('Tom' , 56100.00, 103),
+('Yana', 60200.00, 101);
+
+#EX_2
+CREATE TABLE manufacturers(
+manufacturer_id INT AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(50) NOT NULL UNIQUE,
+established_on DATETIME
+);
+
+CREATE TABLE models (
+model_id INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50) NOT NULL,
+manufacturer_id INT NOT NULL,
+CONSTRAINT fk_manufaturer_id
+FOREIGN KEY (manufacturer_id)
+REFERENCES manufacturers(manufacturer_id)
+)AUTO_INCREMENT=101;
+
+INSERT INTO manufacturers(`name`, established_on) VALUES
+('BMW', '1916-03-01'),
+('Tesla','2003-01-01'),
+('Lada','1966-05-01');
+
+INSERT INTO models (`name`,manufacturer_id )VALUES
+('X1',1),
+('i6',1),
+('Model S',2),
+('Model X',2),
+('Model 3', 2),
+('Nova',3);
+
+#EX_3
+CREATE TABLE students (
+student_id INT AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(50) NOT NULL);
+
+CREATE TABLE exams (
+exam_id INT AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(50) NOT NULL) AUTO_INCREMENT=101;
+
+CREATE TABLE students_exams(
+student_id INT NOT NULL,
+exam_id INT NOT NULL,
+PRIMARY KEY (student_id, exam_id),
+CONSTRAINT fk_students_exams_students
+FOREIGN KEY (student_id)
+REFERENCES students(student_id),
+CONSTRAINT fk_students_exams_exams
+FOREIGN KEY (exam_id)
+REFERENCES exams(exam_id)
+);
+
+INSERT INTO students (`name`) VALUES
+('Mila'),
+('Toni'),
+('Ron');
+
+INSERT INTO exams(`name`) VALUES
+('Spring MVC'),
+('Neo4j'),
+('Oracle 11g');
+
+INSERT INTO students_exams()VALUES
+(1,101),
+(1,102),
+(2,101),
+(3,103),
+(2,102),
+(2,103);
+
+#EX_4
+CREATE TABLE teachers (
+teacher_id INT  AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(50) NOT NULL,
+manager_id INT DEFAULT NULL
+ )
+AUTO_INCREMENT=101;
+
+INSERT INTO teachers (`name`, manager_id) VALUES
+('John',101),
+('Maya',106),
+('Silvia',106),
+('Ted',105),
+('Mark',101),
+('Greta',101);
+
+ALTER TABLE teachers
+ADD CONSTRAINT fk_manager
+FOREIGN KEY (manager_id)
+REFERENCES teachers(teacher_id);
+
+#EX_5
+CREATE TABLE cities (
+city_id INT AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(50)
+);
+
+CREATE TABLE customers (
+customer_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(50),
+birthday DATE,
+city_id INT(11),
+CONSTRAINT fk_city
+FOREIGN KEY (city_id)
+REFERENCES cities(city_id)
+);
+
+CREATE TABLE orders(
+order_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+customer_id INT(11),
+CONSTRAINT fk_customers
+FOREIGN KEY (customer_id)
+REFERENCES customers(customer_id)
+ );
+
+CREATE TABLE item_types(
+item_type_id INT (11) AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(50)
+);
+CREATE TABLE items (
+item_id INT AUTO_INCREMENT PRIMARY KEY, 
+`name` VARCHAR(50),
+item_type_id INT (11),
+CONSTRAINT fk_item_type
+FOREIGN KEY (item_type_id)
+REFERENCES item_types(item_type_id)
+);
+
+CREATE TABLE order_items (
+order_id INT (11) NOT NULL,
+item_id INT (11) NOT NULL,
+PRIMARY KEY (order_id, item_id),
+CONSTRAINT fk_orders
+FOREIGN KEY (order_id)
+REFERENCES orders(order_id),
+CONSTRAINT fk_items
+FOREIGN KEY (item_id)
+REFERENCES items(item_id)
+);
+
+-- EX_6
+CREATE TABLE subjects(
+subject_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+subject_name VARCHAR(50)
+);
+
+CREATE TABLE majors(
+major_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(50)
+); 
+
+CREATE TABLE students(
+student_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+student_number VARCHAR(12),
+student_name VARCHAR(50),
+major_id INT(11),
+CONSTRAINT fk_majors
+FOREIGN KEY (major_id)
+REFERENCES majors(major_id)
+);
+
+CREATE TABLE payments(
+payment_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+payment_date DATE,
+payment_amount DECIMAL(8,2),
+student_id INT(11),
+CONSTRAINT fk_stundets_payments
+FOREIGN KEY (student_id)
+REFERENCES students (student_id)
+);
+
+
+CREATE TABLE agenda(
+student_id INT(11),
+subject_id INT(11),
+Primary KEY (student_id,subject_id),
+CONSTRAINT fk_students
+FOREIGN KEY (student_id)
+REFERENCES students(student_id),
+ CONSTRAINT fk_subjects
+ FOREIGN KEY (subject_id)
+ REFERENCES subjects(subject_id)
+);
+
+
+#EX_9
+SELECT mountain_range, peak_name,(p.elevation)AS peak_elevation
+FROM mountains AS m
+JOIN peaks AS p
+ON m.id=p.mountain_id
+WHERE m.mountain_range='Rila'
+ORDER BY  peak_elevation DESC;
