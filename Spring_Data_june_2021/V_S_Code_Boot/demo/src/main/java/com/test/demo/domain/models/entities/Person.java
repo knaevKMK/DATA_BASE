b2e2e153dc96926266d2d595e48f8e5f1eae4762
knaevKMK <kneav@students.softuni.bg>
2021-07-24 00:37:31 +0300

    vs_code test Spring-Boot
    read from json files

---
+++ Spring_Data_june_2021/V_S_Code_Boot/demo/src/main/java/com/test/demo/domain/models/entities/Person.java
@@ -0,0 +1,7 @@
+import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class Person {
+
+}
---
+++ Spring_Data_june_2021/V_S_Code_Boot/test_vs_code/test/src/main/java/test/vscode/test/models/entities/Person.java
@@ -0,0 +1,8 @@
+package test.vscode.test.models.entities;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+}
